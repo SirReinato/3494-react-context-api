e0512316f9068da968046b8e301c0008ae4104d7
Renato França <renatofranca2881@gmail.com>
2024-01-18 19:48:45 -0300

    com o carrinho funcionando no context

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function addProduto(novoProduto) {
+    const jaTemOProduto = carrinho.some((itemDoCarrinho) => {
+      itemDoCarrinho.id === novoProduto.id;
+    });
+
+    if (!jaTemOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+
+    setCarrinho((carrinhoAnterior) =>
+      carrinhoAnterior.map((itemDoCarrinho) => {
+        if (itemDoCarrinho.id === novoProduto.id) {
+          itemDoCarrinho.quantidade += 1;
+          return itemDoCarrinho;
+        }
+      })
+    );
+  }
+
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
@@ -12,7 +38,7 @@ const Produtos = ({ adicionarProduto }) => {
           <Produto
             key={produto.id}
             {...produto}
-            adicionarProduto={adicionarProduto}
+            adicionarProduto={addProduto}
           />
         ))}
       </div>
